af074596b09744ccbb1bc6d2ec7dd4a31247f982
Adrian Pijoan <adrianpijoan@Adrians-MacBook-Air.local>
2023-03-29 20:25:01 -0600

    first impedence test code

---
+++ ForestNet/src/ForestNet.ino
@@ -0,0 +1,47 @@
+/*
+ * Project ForestNet
+ * Description: A plant network communication system
+ * Author: Adrian Pijoan
+ * Date: 29-MAR-2023
+ */
+
+const int PULSEPIN = A1;
+const int PLANTREADPIN = A2;
+int hz, startTime;
+float plantReading;
+
+void setup() {
+
+  Serial.begin(9600);
+  waitFor(Serial.isConnected, 10000);
+
+  pinMode(PULSEPIN, OUTPUT);
+  pinMode(PLANTREADPIN, INPUT);
+
+  hz = 500; //initialize wave frequency at 500hz
+
+}
+
+void loop() {
+
+  /* Test code to measure plant impedence and see the range of values.
+  Measures from 500-10000hz in intervals of 250 hz once a second */
+  if(startTime - millis() >= 1000){
+    if(hz <= 10000){
+      analogWrite(PULSEPIN, 127, hz);   //syntax for setting frequency of PWM pin -- analogWrite(pin, value, frequency);
+
+      plantReading = analogRead(PLANTREADPIN);
+      
+      Serial.printf("Reading from plant: %.4f at %i hz\n\n", plantReading, hz);
+
+      hz = hz + 250; //increase by 250 hz
+
+    }
+
+    else{
+      printf("Done\n");
+    }
+
+  }
+
+}
